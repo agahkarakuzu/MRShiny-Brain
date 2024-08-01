bea011fc15b719dd2574f60c59116484500b8ed8
Agah <agahkarakuzu@gmail.com>
2024-08-01 10:04:38 -0400

    try remotes

--- init.R
+++ init.R
@@ -1,9 +1,10 @@
 
-my_packages = c("devtools","shiny", "shinythemes", "ggExtra", "plotly", "devtools", "dplyr", "ggcorrplot","tidyverse","terra","bslib","shinyWidgets","thematic","showtext","ragg","ggplot2","DT","gghalves","see","markdown","reshape2")
+update.packages(repos='http://cran.rstudio.com', ask=FALSE, checkBuilt=TRUE)
+my_packages = c("remotes","shiny", "shinythemes", "ggExtra", "plotly", "devtools", "dplyr", "ggcorrplot","tidyverse","terra","bslib","shinyWidgets","thematic","showtext","ragg","ggplot2","DT","gghalves","see","markdown","reshape2")
 install_if_missing = function(p) {
 if (p %in% rownames(installed.packages()) == FALSE) {
 install.packages(p)
 }}
 invisible(sapply(my_packages, install_if_missing))
-
+library(remotes)
 remotes::install_github("jonclayden/RNifti")
