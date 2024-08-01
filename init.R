29f76b0bdba90172b5955a18d3c8da6e950a5dd1
Agah <agahkarakuzu@gmail.com>
2024-08-01 09:48:38 -0400

    packages

--- init.R
+++ init.R
@@ -1,8 +1,9 @@
 
-my_packages = c("shiny", "shinythemes", "RNifti", "ggExtra", "plotly", "devtools", "dplyr", "ggcorrplot","tidyverse","terra","bslib","shinyWidgets","thematic","showtext","ragg","ggplot2","DT","gghalves","see","markdown","reshape2")
+my_packages = c("devtools","shiny", "shinythemes", "ggExtra", "plotly", "devtools", "dplyr", "ggcorrplot","tidyverse","terra","bslib","shinyWidgets","thematic","showtext","ragg","ggplot2","DT","gghalves","see","markdown","reshape2")
 install_if_missing = function(p) {
 if (p %in% rownames(installed.packages()) == FALSE) {
 install.packages(p)
-}
-}
+}}
 invisible(sapply(my_packages, install_if_missing))
+
+remotes::install_github("jonclayden/RNifti")
